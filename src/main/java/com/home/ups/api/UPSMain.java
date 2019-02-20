193bf8e68abeb9a44c02649a998935d442f0ce44
Dinesh Sapkota <Dinesh.Sapkota@JuicePlus.com>
2019-02-20 16:24:06 -0600

    Code optimization

---
+++ src/main/java/com/home/ups/api/UPSMain.java
@@ -0,0 +1,11 @@
+package com.home.ups.api;
+
+public class UPSMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ReturnLabelHttpPost.printLabelMI();
+	}
+
+}
